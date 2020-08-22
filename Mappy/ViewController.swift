ba4656f9afa2e1cbb97d888ed1c898d84964e7ef
Gungor Basa <gungorbasa@gmail.com>
2020-08-22 07:50:36 +0300

    Initial Commit

---
+++ Mappy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Mappy
+//
+//  Created by Gungor Basa on 8/22/20.
+//  Copyright © 2020 Gungor Basa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
